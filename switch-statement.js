cf9e0453c0dd699aad9b41d1f3e9c0fecd28e7af
Mufti Muhammad Nur Prabowo <muftimuhammad97@gmail.com>
2021-09-21 13:48:36 +0700

    update

--- switch-statement.js
+++ switch-statement.js
@@ -14,3 +14,19 @@ switch (nilai) {
     default:
         document.writeln("<p>Mungkin Anda salah jurusan</p>")
 }
+
+const nilaiMu = prompt("Berapa nilai mu = ");
+switch (true) {
+    case (nilaiMu>80):
+        document.writeln("<p>Wow Anda lulus dengan baik</p>");
+        break;
+    case (nilaiMu>70):
+    case (nilaiMu>60):
+        document.writeln("<p>Anda lulus</p>");
+        break;
+    case (nilaiMu>50):
+        document.writeln("<p>Anda tidak lulus</p>");
+        break;
+    default:
+        document.writeln("<p>Mungkin Anda salah jurusan</p>")
+}
